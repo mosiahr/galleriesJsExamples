11f45a9ae4440d9bf94ec72eb4488ba84c976433
mosiahr <grishamosya@gmail.com>
2017-08-10 01:38:57 +0300

    finished scrip1

--- js/script1.js
+++ js/script1.js
@@ -1,3 +1,26 @@
 /**
  * Created by mosya on 09.08.17.
  */
+
+;(function gallery(){
+    // var leftArrow = document.getElementById('leftArrow'),
+    //     rightArrow = document.getElementById('rightArrow'),
+    //     image = document.getElementById('image');
+    var initImgNumber = 1,
+        maxImgNumber = 7;
+
+    leftArrow.addEventListener('click', moveBackward);
+    rightArrow.addEventListener('click', moveForward);
+
+    function moveBackward(){
+        initImgNumber--;
+        if(initImgNumber == 0){initImgNumber = maxImgNumber}
+        image.setAttribute('src', 'img/' + initImgNumber + '.jpg');
+    };
+
+    function moveForward(){
+        initImgNumber++;
+        if(initImgNumber > maxImgNumber){initImgNumber = 1}
+        image.setAttribute('src', 'img/' + initImgNumber + '.jpg');
+    };
+})();
